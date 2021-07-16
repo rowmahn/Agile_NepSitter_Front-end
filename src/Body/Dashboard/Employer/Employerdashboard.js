35c4ae1de618d8186c69f92387affe90d4357edb
unknown <dasdipak98047@gmail.com>
2021-07-16 10:34:18 +0545

    show my booking

--- src/Body/Dashboard/Employer/Employerdashboard.js
+++ src/Body/Dashboard/Employer/Employerdashboard.js
@@ -1,23 +1,102 @@
-import React, { Component,log } from 'react'
+import React, { Component,log} from 'react'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-
+import axios from 'axios';
 import {Redirect} from 'react-router-dom'
+import {Table,Button,Modal,ModalHeader, ModalBody, ModalFooter, 
+    Input, FormGroup
+
+} from 'reactstrap'
 
 export default class Employerdashboard extends Component {
+    state = {
+        isDeleted:false,
+        isEdit:false,
+        hires : [],
+        config:{
+            headers:{'Authorization':'Bearer ' + localStorage.getItem('token')}
+        },
+        // currenthire:{}
+        
+    }
+
+    // toggle = (e) => {
+    //     this.setState({
+    //         isEdit: !this.state.isEdit
+    //     })
+    // }
+
+    componentDidMount(){
+
+        axios.get("http://localhost:90/getmybooking" ,this.state.config)
+        .then((response)=>{
+            
+            console.log(response.data)
+            this.setState({
+                hires : response.data.data
+            })
+        })
+        .catch((err)=>{
+            console.log(err.response)
+        })
+    
+    }
+
     logout=()=> log(
         localStorage.removeItem('token'),
         window.location.href='/',
         alert("successfully logout!!")
       )
     render() {
-        // if(!localStorage.getItem('token')){
-        //     alert("unauthorized Access!!")
-        //     return <Redirect to='/login'/>
-        // }
+        if(!localStorage.getItem('token')){
+            alert("unauthorized Access!!")
+            return <Redirect to='/login'/>
+        }
         return (
+            <div>
             <div className="">
                 <h1 className="text-primary">Employer dashboard comming soon..</h1>
                 <button className="btn-logout" id="logout" onClick={this.logout}>Logout<ExitToAppIcon className="ml-2"/></button>
+            </div>
+            <div>
+                    <h2>My bookings</h2>
+
+                    <Table striped bordered>
+<thead>
+<tr>
+<th>S.No</th>
+<th>Location</th>
+<th>Date</th>
+<th>Day</th>
+<th>Hours</th>
+<th>Package</th>
+
+</tr>
+</thead>
+<tbody>
+  {
+this.state.hires.map((hire, i)=>{
+ return (
+    
+                     <tr>
+                        <th scope="row">{i+1}</th>
+                        <td>{ hire.Location }</td>
+                        <td>{hire.Date}</td>
+                        <td>{hire.Day}</td>
+                        <td>{hire.Hours}</td>
+                        <td> { hire.Package}</td>
+                        {/* <td><button onClick={()=>{
+                if(window.confirm('Are you sure to delete this E-Tender Form'))
+                        this.handleDelete(hire._id)}} >Delete</button>            
+                         <button onClick={()=>this.HireEdit(hire._id)}>Update</button>
+                       </td> */}
+                      </tr>
+                        )
+                      })
+                     }
+              </tbody>
+           </Table>
+
+            </div>
             </div>
         )
     }
