098b7bd58cc10249409672b8fc3769ee1cdd0231
Aatish05 <aatishrestha05@gmail.com>
2021-07-23 18:09:04 +0545

    design dashboard and forms

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,17 +3,12 @@ import { Component} from "react";
 import React, { useState } from 'react';
 
 import {
-    Form,
-    Input,
-    Tooltip,
-    Cascader,
-    Select,
-    Button,Upload,Menu, Dropdown,
-    AutoComplete,
-    Modal
+    Button,
+    Menu,
+    Dropdown
+
   } from 'antd';
   import "antd/dist/antd.css";
-  import { QuestionCircleOutlined,UploadOutlined, } from '@ant-design/icons';
 
 
 
@@ -83,13 +78,7 @@ class Header extends Component{
                     
                   </ul>
                   <div class="navbar-nav">
-                  {/* <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="/register">Signup</a>
-                    </li>
-                    <a class="nav-link ">||</a>
-                    <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="#">Login</a>
-                    </li> */}
+                
                      <div class="navbar-nav">
                        <li class="nav-item">
                            <a class="nav-link " aria-current="page" href="/register">Signup</a>
@@ -113,60 +102,6 @@ class Header extends Component{
       
     
 }
-//     render(){
-//       return(
-      
-//         <div className='header'class="header fixed-top"> 
-//         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
-//                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>
-//                <div>
-//                <nav class="navbar navbar-expand-lg navbar-light ">
-//                   <div class="container-fluid">
-//                     <a class="navbar-brand" href="/">NepSitter</a>
-//                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"navbar-fixed-top>
-//                       <span class="navbar-toggler-icon"></span>
-//                     </button>
-//                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//                       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-//                         <li class="nav-item">
-//                           <a class="nav-link " aria-current="page" href="/">Home</a>
-//                         </li>
-//                         <li class="nav-item">
-//                           <a class="nav-link " aria-current="page" href="#">Apply for Job</a>
-//                         </li>
-//                         <li class="nav-item">
-//                           <a class="nav-link " aria-current="page" href="/register">Our Services</a>
-//                         </li>
-                        
-//                       </ul>
-//                       <div class="navbar-nav">
-//                       <li class="nav-item">
-//                           <a class="nav-link " aria-current="page" href="/register">Signup</a>
-//                         </li>
-//                         <a class="nav-link ">||</a>
-//                         <li class="nav-item">
-//                           <a class="nav-link " aria-current="page" href="#">Login</a>
-//                         </li>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </nav>
-//                </div>
-                
-//             </div>
-
-
-      
-     
-
-       
-//         )
-// }
-
-
-        
-        
-    
 
 
 export default Header;
