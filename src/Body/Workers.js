eb7461462d425156d26e8c9baefd9a339e3bb5fb
unknown <dasdipak98047@gmail.com>
2021-07-25 18:31:28 +0545

    modified search

--- src/Body/Workers.js
+++ src/Body/Workers.js
@@ -1,10 +1,13 @@
 import React, { Component , state} from 'react'
 import  axios from 'axios'
 import { Link } from 'react-router-dom'
+import { FormGroup } from 'react-bootstrap'
 
 export default class Workers extends Component {
     state={
-        workers:[]
+        workers:[],
+        search:'',
+        
     }
     componentDidMount(){
         axios.get("http://localhost:90/showworkers/all")
@@ -19,29 +22,56 @@ export default class Workers extends Component {
             console.log(err.response)
         })
     }
+    fetchworker=(query,e)=>{
+        // e.preventDefault();
+        axios.get('http://localhost:90/search/'+query)
+        .then(result=>{
+         console.log(result)
+        })
+        .catch(err=>{
+          console.log(err)
+           
+        })
+      }
 
     render() {
         return (
             <div>
-                <div class="row">
+                <div className="row">
+                <form  className="inline searchform ">
+                    <FormGroup>
+                    <input className="autocomplete-input col-6 " type="text" 
+                    name="Search"
+                      placeholder="Search"
+                      aria-label="Search "
+                      value={this.state.Search}
+                      onChange=
+                      {(event)=>{this.setState({Search:event.target.value})}}
+                    />
+                    <button name="searchbutton" className="col-2" onClick={this.fetchworker.bind(this.state.Search)} >Search</button>
+                    </FormGroup>
+                    </form>
+                </div>
+
+                <div className="row">
                     {
                         this.state.workers.map((worker)=>{
                             return(
-                <div class="card" style={{width: '18rem'}}>
-                    <img class="card-img-top"
+                <div className="card" style={{width: '18rem'}}>
+                    <img className="card-img-top"
                      src="https://static.wikia.nocookie.net/disney/images/f/f2/Miley_Cyrus.jpg/revision/latest/top-crop/width/360/height/450?cb=20200805232558"
                     //  {"http://localhost:90/Images/"+ product.productpic} alt={product.productpic} 
                      alt="Card image cap"/>
-                    <div class="card-body">
-                        <p class="card-title">{worker.fname} {worker.lname}</p>
+                    <div className="card-body">
+                        <p className="card-title">{worker.fname} {worker.lname}</p>
                         <div className="price">
                             
                             <p>{worker.phone} </p>
-                        <p class="card-text">{Worker.address}</p>
+                        <p className="card-text">{Worker.address}</p>
                            <p>{worker.city}</p> 
                         
                         </div>
-                        <p class="card-text">{worker.jobcategory}</p>
+                        <p className="card-text">{worker.jobcategory}</p>
                     </div>
                    
                       <Link to={'/workerdetails/'+worker._id} className="btn btn-primary">Worker Details</Link>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,121 +1,123 @@
-import { Component} from "react";
+import { Component } from "react";
 
-import React, { useState } from 'react';
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
 
 import {
-    Form,
-    Input,
-    Tooltip,
-    Cascader,
-    Select,
-    Button,Upload,Menu, Dropdown,
-    AutoComplete,
-    Modal
-  } from 'antd';
-  import "antd/dist/antd.css";
-  import { QuestionCircleOutlined,UploadOutlined, } from '@ant-design/icons';
+ 
+  Button,  Menu, Dropdown,
+  
+} from 'antd';
+
+import "antd/dist/antd.css";
 
 
 
-class Header extends Component{
-  render(){
-    
+class Header extends Component {
+  state = {
 
-      const menu = (
-        <Menu>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Baby Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Pet Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              House Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Nanny
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Senior Care
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Granny Storytime
-            </a>
-          </Menu.Item>
-        </Menu>
-      );
+    worker:[],
+    Search: ''
+  }
+ 
+  render() {
 
- return (
-          <div>
-            //         <div className='header'class="header fixed-top"> 
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
-                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>               <div></div>
 
-              <nav class="navbar navbar-expand-lg navbar-light ">
-              <div class="container-fluid">
-                <a class="navbar-brand" href="/">NepSitter</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"navbar-fixed-top>
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="/applyforjob">Apply for Job</a>
-                    </li>
-                    
-                    <Dropdown overlay={menu} className="service" placement="bottomRight" arrow>
-                     <Button>Our Services</Button>
-                     </Dropdown>
-                    
-                  </ul>
+    const menu = (
+      <Menu>
+        <Menu.Item>
+          <NavLink target="_blank" rel="noopener noreferrer" to="">
+            Baby Sitter
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink target="_blank" rel="noopener noreferrer" to="">
+            Pet Sitter
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink target="_blank" rel="noopener noreferrer" to="">
+            House Sitter
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink target="_blank" rel="noopener noreferrer" to="">
+            Nanny
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink target="_blank" rel="noopener noreferrer" to="">
+            Senior Care
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item>
+          <NavLink target="_blank" rel="noopener noreferrer" to="">
+            Granny Storytime
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+    );
+
+    return (
+      <div>
+            //         <div className='header' class="header fixed-top">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>               <div></div>
+
+          <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="container-fluid">
+              <NavLink class="navbar-brand" to="/">NepSitter</NavLink>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" navbar-fixed-top>
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <NavLink class="nav-link " aria-current="page" to="/">Home</NavLink>
+                  </li>
+                  <li class="nav-item">
+                    <NavLink class="nav-link " aria-current="page" to="/applyforjob">Apply for Job</NavLink>
+                  </li>
+                  <li class="nav-item">
+                    <NavLink class="nav-link " aria-current="page" to="/workers">Show workers</NavLink>
+                  </li>
+                  <Dropdown overlay={menu} className="service" placement="bottomRight" arrow>
+                    <Button>Our Services</Button>
+                  </Dropdown>
+
+                </ul>
+                <div class="navbar-nav">
+                
                   <div class="navbar-nav">
-                  {/* <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="/register">Signup</a>
-                    </li>
-                    <a class="nav-link ">||</a>
+
                     <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="#">Login</a>
-                    </li> */}
-                     <div class="navbar-nav">
-                       <li class="nav-item">
-                           <a class="nav-link " aria-current="page" href="/register">Signup</a>
-                         </li>
-                         <a class="nav-link ">||</a>
-                         <li class="nav-item">
-                           <a class="nav-link " aria-current="page" href="/login">Login</a>
-                         </li>
-                         
-                      </div>
+                      <NavLink class="nav-link " aria-current="page" to="/register">Signup</NavLink>
+                    </li>
+                    <NavLink class="nav-link " to="">||</NavLink>
+                    <li class="nav-item">
+                      <NavLink class="nav-link " aria-current="page" to="/login">Login</NavLink>
+                    </li>
+
                   </div>
                 </div>
               </div>
-            </nav>
-        
-          </div>
-          </div>
-            
-        );
-      };
-      
-    
+            </div>
+          </nav>
+
+        </div>
+
+      </div>
+
+
+    );
+  };
+
+
 }
 //     render(){
 //       return(
-      
+
 //         <div className='header'class="header fixed-top"> 
 //         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
 //                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>
@@ -137,7 +139,7 @@ class Header extends Component{
 //                         <li class="nav-item">
 //                           <a class="nav-link " aria-current="page" href="/register">Our Services</a>
 //                         </li>
-                        
+
 //                       </ul>
 //                       <div class="navbar-nav">
 //                       <li class="nav-item">
@@ -152,21 +154,21 @@ class Header extends Component{
 //                   </div>
 //                 </nav>
 //                </div>
-                
+
 //             </div>
 
 
-      
-     
 
-       
+
+
+
 //         )
 // }
 
 
-        
-        
-    
+
+
+
 
 
 export default Header;
