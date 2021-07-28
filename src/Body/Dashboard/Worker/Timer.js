9a68f9562140ec32ff749686cc981888241f2f2c
rowmahn <dulalroman1@gmail.com>
2021-07-28 19:34:30 +0545

    timer updated

--- src/Body/Dashboard/Worker/Timer.js
+++ src/Body/Dashboard/Worker/Timer.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import DisplayComponent from './DisplayComponent';
 import BtnComponent from './BtnComponent';
 import '../../../style/timer.css'
+import axios from 'axios';
 
 
 
@@ -9,21 +10,26 @@ function Timer() {
   const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
   const [interv, setInterv] = useState();
   const [status, setStatus] = useState(0);
+  const [config,setConfig] = useState({headers:{'Authorization':'Bearer ' + localStorage.getItem('token')}})
+  var hr = 0;
   // Not started = 0
   // started = 1
   // stopped = 2
 
   const start = () => {
     run();
+
     setStatus(1);
     setInterv(setInterval(run, 10));
   };
 
-  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;hr= time.h;
 
   const run = () => {
+    
     if(updatedM === 60){
       updatedH++;
+      hr++
       updatedM = 0;
     }
     if(updatedS === 60){
@@ -32,15 +38,35 @@ function Timer() {
     }
     if(updatedMs === 100){
       updatedS++;
+      
       updatedMs = 0;
     }
     updatedMs++;
-    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH,hr:hr});
+    
+    
   };
+
 
   const stop = () => {
     clearInterval(interv);
     setStatus(2);
+    //console.log(sec)
+    
+    axios.post("http://localhost:90/timer/"+hr,config)
+    
+        .then(response=>{
+            //console.log(response.data)
+            
+            alert("Your time has been tracked")
+
+               
+        })
+        .catch(err=>{
+            console.log(err);
+            
+        })
+
   };
 
   const reset = () => {
@@ -58,6 +84,7 @@ function Timer() {
           <div className="stopwatch">
                <DisplayComponent time={time}/>
                <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+              
           </div>
      </div>
     </div>
