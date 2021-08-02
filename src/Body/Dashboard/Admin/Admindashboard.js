1b0d314e22124ed32cb0fe325d7eaa76d3a2bc55
Aatish05 <aatishrestha05@gmail.com>
2021-08-02 16:05:27 +0545

    worker profile created

--- src/Body/Dashboard/Admin/Admindashboard.js
+++ src/Body/Dashboard/Admin/Admindashboard.js
@@ -1,26 +1,19 @@
-import React, { Component,log} from 'react'
+import React, { Component } from 'react'
 import '../../../style/nav.css'
-export default class Employerdashboard extends Component {
-    logout=()=> log(
-        localStorage.removeItem('token'),
-        window.location.href='/',
-        alert("successfully logout!!")
-      )
+export default class Admindashboard extends Component {
     render() {
         return (
             <div>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link> 
-                <div class="wrapper">
+                 <div class="wrapper">
                     <div class="sidebar">
                         <h2>NepSitter</h2>
                         <ul>
                             <li><a href="#"><i class="fas fa-home"></i>User</a></li>
                             <li><a href="#"><i class="fas fa-user"></i>Profile</a></li>
-                            <li><a href="workers"><i class="fas fa-user-md"></i>Services</a></li>
+                            <li><a href="#"><i class="fas fa-user-md"></i>Services</a></li>
                             <li><a href="#"><i class="fas fa-address-card"></i>My Data</a></li>
-                            <li><a href="#"><i class="fas fa-bell"></i>Notification</a></li>
                             <li><a href="#"><i class="fas fa-history"></i>History</a></li>
-                            <li><a href="#" onClick={this.logout}><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+                            <li><a href="#"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
                         </ul> 
                         <div class="social_media">
                         <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -38,4 +31,3 @@ export default class Employerdashboard extends Component {
         )
     }
 }
-
--- src/Body/Dashboard/Employer/Employerdashboard.js
+++ src/Body/Dashboard/Employer/Employerdashboard.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component,log} from 'react'
 import '../../../style/nav.css'
 export default class Employerdashboard extends Component {
+    logout=()=> log(
+        localStorage.removeItem('token'),
+        window.location.href='/',
+        alert("successfully logout!!")
+      )
     render() {
         return (
             <div>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link> 
                 <div class="wrapper">
                     <div class="sidebar">
                         <h2>NepSitter</h2>
                         <ul>
                             <li><a href="#"><i class="fas fa-home"></i>User</a></li>
                             <li><a href="#"><i class="fas fa-user"></i>Profile</a></li>
-                            <li><a href="#"><i class="fas fa-user-md"></i>Services</a></li>
+                            <li><a href="workers"><i class="fas fa-user-md"></i>Services</a></li>
                             <li><a href="#"><i class="fas fa-address-card"></i>My Data</a></li>
+                            <li><a href="#"><i class="fas fa-bell"></i>Notification</a></li>
                             <li><a href="#"><i class="fas fa-history"></i>History</a></li>
-                            <li><a href="#"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+                            <li><a href="#" onClick={this.logout}><i class="fas fa-sign-out-alt"></i>Logout</a></li>
                         </ul> 
                         <div class="social_media">
                         <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -31,3 +38,4 @@ export default class Employerdashboard extends Component {
         )
     }
 }
+
