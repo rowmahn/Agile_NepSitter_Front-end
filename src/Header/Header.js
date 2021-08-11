ffe48e2ce5d9fae19aa252ae3877a1337f82fe13
unknown <dasdipak98047@gmail.com>
2021-08-11 15:43:32 +0545

    navigation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -15,64 +15,51 @@ import {
 
 
 class Header extends Component{
+ 
   render(){
-    
+    const menu = (
+      <Menu>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Baby Sitter
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Pet Sitter
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            House Sitter
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Nanny
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Senior Care
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Granny Storytime
+          </a>
+        </Menu.Item>
+      </Menu>
+    );
     if(localStorage.getItem('token'))
     {
-      var menu=
+      var nav=
       <div>  
       </div>
     }else{
-      const menu = (
-        <Menu>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Baby Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Pet Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              House Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Nanny
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Senior Care
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Granny Storytime
-            </a>
-          </Menu.Item>
-        </Menu>
-      );
-
-    }
-
-
-
-     
-
- return (
-          <div>
-            {menu}
-
-            //         <div className='header'class="header fixed-top"> 
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
-                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>               <div></div>
-
-              <nav class="navbar navbar-expand-lg navbar-light ">
+      
+var nav=<div>
+   <nav class="navbar navbar-expand-lg navbar-light ">
               <div class="container-fluid">
                 <a class="navbar-brand" href="/">NepSitter</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"navbar-fixed-top>
@@ -111,8 +98,22 @@ class Header extends Component{
             
           </nav>
 
-        </div>
+</div>
+    }
 
+
+
+     
+
+ return (
+          <div>
+            <div className='header'class="header fixed-top"> 
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
+                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>               <div></div>
+                 {nav}
+             
+        </div>
+            
       </div>
 
 
