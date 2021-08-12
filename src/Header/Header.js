d32efde928528f731ad64d8c3d76abf12e1cb226
rowmahn <dulalroman1@gmail.com>
2021-08-12 19:23:31 +0545

    header made similar as navaigation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,8 @@
-import { Component} from "react";
+import { Component } from "react";
 
-import React, { useState } from 'react';
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
 
 import {
     Button,
@@ -13,64 +15,51 @@ import {
 
 
 class Header extends Component{
+ 
   render(){
-    
+    const menu = (
+      <Menu>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Baby Sitter
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Pet Sitter
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            House Sitter
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Nanny
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Senior Care
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="">
+            Granny Storytime
+          </a>
+        </Menu.Item>
+      </Menu>
+    );
     if(localStorage.getItem('token'))
     {
-      var menu=
+      var nav=
       <div>  
       </div>
     }else{
-      const menu = (
-        <Menu>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Baby Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Pet Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              House Sitter
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Nanny
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Senior Care
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="">
-              Granny Storytime
-            </a>
-          </Menu.Item>
-        </Menu>
-      );
-
-    }
-
-
-
-     
-
- return (
-          <div>
-            {menu}
-
-            //         <div className='header'class="header fixed-top"> 
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
-                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>               <div></div>
-
-              <nav class="navbar navbar-expand-lg navbar-light ">
+      
+var nav=<div>
+   <nav class="navbar navbar-expand-lg navbar-light ">
               <div class="container-fluid">
                 <a class="navbar-brand" href="/">NepSitter</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"navbar-fixed-top>
@@ -79,8 +68,9 @@ class Header extends Component{
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                      <a class="nav-link " aria-current="page" href="/">Home</a>
+                      <NavLink class="nav-link " aria-current="page" to="/register">Signup</NavLink>
                     </li>
+                    <NavLink class="nav-link " to="">||</NavLink>
                     <li class="nav-item">
                       <a class="nav-link " aria-current="page" href="/applyforjob">Apply for Job</a>
                     </li>
@@ -105,15 +95,32 @@ class Header extends Component{
                   </div>
                 </div>
               </div>
-            </nav>
-        
-          </div>
-          </div>
             
-        );
-      };
-      
-    
+          </nav>
+
+</div>
+    }
+
+
+
+     
+
+ return (
+          <div>
+            <div className='header'class="header fixed-top"> 
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></link>
+                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"></link>               <div></div>
+                 {nav}
+             
+        </div>
+            
+      </div>
+
+
+    );
+  };
+
+
 }
 
 
