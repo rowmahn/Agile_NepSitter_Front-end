b078a003fee5a803cdf7f0143abcfc620b80f39a
rowmahn <dulalroman1@gmail.com>
2021-08-20 16:46:31 +0545

    filter search design completed

--- src/Body/Dashboard/Employer/Filtersearch.js
+++ src/Body/Dashboard/Employer/Filtersearch.js
@@ -1,4 +1,5 @@
 
+import { Button } from "bootstrap";
 import {Component} from "react"
 import '../../../style/filtersearch.css'
 class Filtersearch extends Component{
@@ -50,9 +51,70 @@ render(){
     <button class="btn btn-primary search" id="search">Search</button>
   </div>
 
+  <div class="container mt-5 d-flex justify-content-center">
+  <div class="card p-4 mt-3">
+        <div class="first">
+            <div class="timee d-flex flex-row align-items-center justify-content-between mt-3">
+                <div class="d-flex align-items-center"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">6 hrs</span> </div>
+                <div> <span class="font-weight-bold">Rs 100</span> </div>
+            </div>
+        </div>
+        <div class="second d-flex flex-row mt-2">
+            <div class="image mr-3"> <img src="https://i.imgur.com/0LKZQYM.jpg" class="rounded-circle" width="60" /> </div>
+            <div class="">
+            <p>Name</p>
+            <p>Gender</p>
+            <p>Location</p>
+            <p>Experience</p>
+                <div class="d-flex flex-row mb-1"> 
+                    <div class="ratings ml-2"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>
+                </div>
+               </div>
+            
+        </div>
+        
+        <div class="third mt-4"> 
+        <button class="btn btn-success btn-block"><i class="fa fa-clock-o"></i> View Profile</button>
+        <button class="btn btn-success btn-block"><i class="fa fa-clock-o"></i> Book Now</button>
+        
+        </div>
+        
+    </div>
+    <div class="card p-4 mt-3">
+        <div class="first">
+            <div class="timee d-flex flex-row align-items-center justify-content-between mt-3">
+                <div class="d-flex align-items-center"> <i class="fa fa-clock-o clock"></i> <span class="hour ml-1">6 hrs</span> </div>
+                <div> <span class="font-weight-bold">Rs 100</span> </div>
+            </div>
+        </div>
+        <div class="second d-flex flex-row mt-2">
+            <div class="image mr-3"> <img src="https://i.imgur.com/0LKZQYM.jpg" class="rounded-circle" width="60" /> </div>
+            <div class="">
+            <p>Name</p>
+            <p>Gender</p>
+            <p>Location</p>
+            <p>Experience</p>
+                <div class="d-flex flex-row mb-1"> 
+                    <div class="ratings ml-2"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>
+                </div>
+               </div>
+            
+        </div>
+        
+        <div class="third mt-4"> 
+        <button class="btn btn-success btn-block"><i class="fa fa-clock-o"></i> View Profile</button>
+        <button class="btn btn-success btn-block"><i class="fa fa-clock-o"></i> Book Now</button>
+        
+        </div>
+        
+    </div>
+    
+</div>
+</div>
+
 
            </div>
-       </div>
+      
     )
 }
 }
