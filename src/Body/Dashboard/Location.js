4bd51e9fa01755d185fd943127327b3f05f58921
unknown <dasdipak98047@gmail.com>
2021-08-22 20:56:26 +0545

    location of marker

--- src/Body/Dashboard/Location.js
+++ src/Body/Dashboard/Location.js
@@ -1,32 +1,18 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import '../../style/style.css'
+import GoogleMap  from './components/GoogleMap'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Location extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
+
 
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyDP7p0HlOio_2iKRURwsY946e8LDP48nKQ'}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Location"
-          />
-        </GoogleMapReact>
+    
+      <div >
+           <div className="loco">Location At google Map</div>
+        <GoogleMap/>
+     
       </div>
     );
   }
