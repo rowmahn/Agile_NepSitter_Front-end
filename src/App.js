0cdcb7b193cf993cc6a9065356a6e797ae56ac9f
unknown <dasdipak98047@gmail.com>
2021-08-24 22:25:07 +0545

    change in appjs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <Header></Header>
         <Body></Body>
-        {/* <Footer></Footer> */}
+        <Footer></Footer>
         
       </div>
     </BrowserRouter>
